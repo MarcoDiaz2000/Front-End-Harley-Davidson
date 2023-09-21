cd2f8d6e8e9646842c0dd60cab0dadfda15291a8
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-21 15:44:01 +0500

    add login functins and install tailwind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+// Import the necessary components
+import HomePage from './pages/home/Homepage';
+import SignUpPage from './pages/signup/SignUp';
+import SignInPage from './pages/signin/SignIn';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App d-flex">
+      <Routes>
+        <Route path="/" element={(<HomePage />)} />
+        <Route path="/signin" element={(<SignInPage />)} />
+        <Route path="/signup" element={(<SignUpPage />)} />
+      </Routes>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
 
