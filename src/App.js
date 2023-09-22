c359c855947a4806223993cb123f27daa48e441c
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-22 06:50:48 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from './components/layout/Layout';
+import Home from './components/pages/Home';
+import Reservations from './components/pages/Reservations';
+import Reserve from './components/pages/Reserve';
+import AddMotor from './components/pages/AddMotor';
+import DeleteMotor from './components/pages/DeleteMotor';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/motors/:id" element={<Home />} />
+          <Route path="/myReservation/:id" element={<Reservations />} />
+          <Route path="/reserve/:id" element={<Reserve />} />
+          <Route path="/add/:id" element={<AddMotor />} />
+          <Route path="/delete/:id" element={<DeleteMotor />} />
+
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
