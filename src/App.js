de11a9f44372a48d4e4be82d3c6fb508ddd1372d
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-22 18:33:49 +0300

    resolve conflict dev branch

--- src/App.js
+++ src/App.js
@@ -1,41 +1,38 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 // Import the necessary components
+import { Routes, Route } from 'react-router-dom';
 import HomePage from './pages/home/Homepage';
 import SignUpPage from './pages/signup/SignUp';
 import SignInPage from './pages/signin/SignIn';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/layout/Layout';
-import Home from './components/pages/Home';
-import Reservations from './components/pages/Reservations';
-import Reserve from './components/pages/Reserve';
-import AddMotor from './components/pages/AddMotor';
-import DeleteMotor from './components/pages/DeleteMotor';
+import Main from './routes/Home';
+import Delete from './routes/Delete';
+import AddNew from './routes/AddNew';
+import MyReservations from './routes/MyReservations';
+import Reserve from './routes/Reserve';
 
 function App() {
   return (
-    <Router>
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+      </Routes>
       <Layout>
         <Routes>
-          <Route path="/motors/:id" element={<Home />} />
-          <Route path="/myReservation/:id" element={<Reservations />} />
-          <Route path="/reserve/:id" element={<Reserve />} />
-          <Route path="/add/:id" element={<AddMotor />} />
-          <Route path="/delete/:id" element={<DeleteMotor />} />
 
+          <Route path="/motors/:id" element={<Main />} />
+          <Route path="/myReservation/:id" element={<MyReservations />} />
+          <Route path="/reserve/:id" element={<Reserve />} />
+          <Route path="/add/:id" element={<AddNew />} />
+          <Route path="/delete/:id" element={<Delete />} />
         </Routes>
       </Layout>
-    </Router>
-    <div className="App d-flex">
-      <Routes>
-        <Route path="/" element={(<HomePage />)} />
-        <Route path="/signin" element={(<SignInPage />)} />
-        <Route path="/signup" element={(<SignUpPage />)} />
-      </Routes>
-    </div>
+    </>
   );
 }
 
---
+++ src/routes/Delete.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Delete() {
+  return (
+    <div>Delete</div>
+  );
+}
