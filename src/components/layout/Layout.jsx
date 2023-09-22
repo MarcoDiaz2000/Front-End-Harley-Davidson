d4623ac139372cc83b3bb8dd62640e497378150e
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-22 06:50:54 +0300

    Create Layout.jsx

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Sidebar from './Sidebar';
+
+export default function Layout({ children }) {
+  return (
+    <div className="flex gap-4 ">
+      <div className="md:w-1/5">
+        <Sidebar />
+      </div>
+      <div className="w-full md:w-4/5 overflow-y-auto">
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
