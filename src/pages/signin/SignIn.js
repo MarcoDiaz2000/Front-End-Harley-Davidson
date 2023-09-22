519a438f60d5e35ada246913fbd4d53275fa97e9
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-22 14:47:22 +0500

    Configre dispatch

--- src/pages/signin/SignIn.js
+++ src/pages/signin/SignIn.js
@@ -1,47 +1,55 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { login } from '../../redux/auth/index';
+import { login } from '../../redux/auth';
 
 const SignInPage = () => {
   const [username, setUsername] = useState('');
+  const [error, setError] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
     setUsername(e.target.value);
   };
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const handleSubmit = (e) => {
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    const user = {
-      name: username,
-    };
-    dispatch(login(user));
-    navigate('/home');
+
+    try {
+      const user = {
+        name: username,
+      };
+
+      await dispatch(login(user));
+      navigate('/home');
+    } catch (error) {
+      setError('Login failed. Please try again.'); // Set an error message to display to the user
+    }
   };
 
   return (
-    <>
-      <div className="container mt-5 ml-1 col-sm-4">
-        <form onSubmit={(e) => handleSubmit(e)} className="form">
-          <h3 className="text-center">
-            LogIn
-          </h3>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="text"
-              name="username"
-              placeholder="User name"
-              onChange={handleChange}
-              value={username}
-            />
-          </div>
-          <button type="submit" className="btn btn-success">
-            LogIn
-          </button>
-        </form>
-      </div>
-    </>
+    <div className="container mt-5 ml-1 col-sm-4 mx-auto">
+      <form onSubmit={handleSubmit} className="form">
+        <h3 className="text-center">LogIn</h3>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="username"
+            placeholder="User name"
+            onChange={handleChange}
+            value={username}
+          />
+        </div>
+        {error && <div className="text-danger">{error}</div>}
+        {' '}
+        {/* Display the error message */}
+        <button className="btn btn-success" type="submit">
+          LogIn
+        </button>
+      </form>
+    </div>
   );
 };
 
--- src/pages/signup/SignUp.js
+++ src/pages/signup/SignUp.js
@@ -1,21 +1,24 @@
-import { React, useState } from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
-import { signup } from '../../redux/auth/index';
+import { signup } from '../../redux/auth';
 
 const SignUpPage = () => {
   const [username, setUsername] = useState('');
-
+  const [error, setError] = useState('');
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const user = {
-      name: username,
-    };
 
-    dispatch(signup(user));
-    navigate('/home');
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await dispatch(signup({ username }));
+
+      navigate('/home');
+    } catch (error) {
+      setError('Signup failed. Please try again.');
+    }
   };
 
   const handleOnChange = (e) => {
@@ -23,26 +26,27 @@ const SignUpPage = () => {
   };
 
   return (
-    <>
-      <div className="container mt-5 ml-1 col-sm-4">
-        <form onSubmit={handleSubmit} className="form m-2">
-          <h3 className="text-center">
-            SignUp
-          </h3>
-          <div className="mb-3">
-            <input
-              className="form-control"
-              type="text"
-              name="username"
-              placeholder="User name"
-              onChange={handleOnChange}
-              value={username}
-            />
-          </div>
-          <button className="btn btn-success" type="submit">signup</button>
-        </form>
-      </div>
-    </>
+    <div className="container mt-5 ml-1 col-sm-4">
+      <form onSubmit={handleSubmit} className="form m-2">
+        <h3 className="text-center">SignUp</h3>
+        <div className="mb-3">
+          <input
+            className="form-control"
+            type="text"
+            name="username"
+            placeholder="User name"
+            onChange={handleOnChange}
+            value={username}
+          />
+        </div>
+        {error && <div className="text-danger">{error}</div>}
+        {' '}
+        {/* Display the error message */}
+        <button className="btn btn-success" type="submit">
+          Signup
+        </button>
+      </form>
+    </div>
   );
 };
 
