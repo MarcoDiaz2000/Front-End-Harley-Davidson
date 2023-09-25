ca6611dd32403e69d07daf620829d330f8e60463
Sumeya <sumeyaibrahim72@gmail.com>
2023-09-25 18:24:46 +0300

    Add layout

--- src/components/motors/Details.jsx
+++ src/components/motors/Details.jsx
@@ -4,6 +4,29 @@ import { useParams } from 'react-router-dom';
 export default function Details() {
   const { id } = useParams();
   return (
-    <div>{id}</div>
+    <div className="w-full h-full flex justify-center gap-2 bg-red-500 px-4 py-4">
+      <div className="flex flex-grow bg-green-500">
+        {id}
+        img
+      </div>
+      <div className="flex flex-col w-80 px-4 py-4 bg-green-500">
+        <div className="header">
+          name
+          deposit
+        </div>
+        <ul className="list">
+          <li>lists</li>
+          <li>lists</li>
+          <li>lists</li>
+          <li>lists</li>
+        </ul>
+        <div>
+          <span>general info</span>
+        </div>
+        <div>
+          <button type="button">Reserve</button>
+        </div>
+      </div>
+    </div>
   );
 }
