292ca5ab4979ad3ecd21e4334f10077e700fe7a7
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-25 20:14:15 +0500

    Add validations

--- src/components/Reservation/ReservationForm.jsx
+++ src/components/Reservation/ReservationForm.jsx
@@ -1,90 +1,92 @@
-import React, { useState } from 'react';
+import React from 'react';
+import {
+  Formik, Form, Field, ErrorMessage,
+} from 'formik';
+import * as Yup from 'yup';
 
-export default function ReservationForm() {
-  const [formData, setFormData] = useState({
+const ReservationForm = () => {
+  const initialValues = {
     name: '',
     email: '',
     date: '',
     time: '',
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  const validationSchema = Yup.object().shape({
+    name: Yup.string().required('Name is required'),
+    date: Yup.date().required('Date is required'),
+    time: Yup.string().required('Time is required'),
+    motorcycle: Yup.string().required('Motorcycle is required'),
+  });
+
+  const handleSubmit = async (values, { resetForm }) => {
     // Handle form submission here, e.g., send data to an API or perform other actions
-    console.log('Form data:', formData);
+    console.log('Form data:', values);
     // Clear form fields
-    setFormData({
-      name: '',
-      email: '',
-      date: '',
-      time: '',
-    });
+    resetForm();
   };
 
   return (
     <div className="container mx-auto p-4">
       <h2 className="text-2xl font-semibold mb-4">Reservation</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="mb-4">
-          <label htmlFor="name" className="block text-gray-600">Name:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="email" className="block text-gray-600">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="date" className="block text-gray-600">Date:</label>
-          <input
-            type="date"
-            id="date"
-            name="date"
-            value={formData.date}
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="time" className="block text-gray-600">Time:</label>
-          <input
-            type="time"
-            id="time"
-            name="time"
-            value={formData.time}
-            onChange={handleChange}
-            required
-            className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-        </div>
-        <button type="submit" className="bg-green-500 text-white px-4 py-2 rounded-md hover:text-green-400 hover:bg-green-600 focus:outline-none">
-          Submit
-        </button>
-      </form>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {() => (
+          <Form>
+            <div className="mb-4">
+              <label htmlFor="name" className="block text-gray-600">Name:</label>
+              <Field
+                type="text"
+                id="name"
+                name="name"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <ErrorMessage name="name" component="div" className="text-red-600" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="date" className="block text-gray-600">Date:</label>
+              <Field
+                type="date"
+                id="date"
+                name="date"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <ErrorMessage name="date" component="div" className="text-red-600" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="time" className="block text-gray-600">Time:</label>
+              <Field
+                type="time"
+                id="time"
+                name="time"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <ErrorMessage name="time" component="div" className="text-red-600" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="motorcycle" className="block text-gray-600">Motorcycle:</label>
+              <Field
+                type="text"
+                id="motorcycle"
+                name="motorcycle"
+                className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <ErrorMessage name="motorcycle" component="div" className="text-red-600" />
+            </div>
+            <button
+              type="submit"
+              className="bg-green-500 text-white px-4 py-2 rounded-md hover:text-green-400 hover:bg-green-600 focus:outline-none"
+            >
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
-}
+};
+
+export default ReservationForm;
