fb4f65b6e2e9b541c7922b507c4ca05627c866bb
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-09-25 21:07:22 +0500

    Add Location

--- src/components/Reservation/ReservationForm.jsx
+++ src/components/Reservation/ReservationForm.jsx
@@ -3,20 +3,32 @@ import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
 import * as Yup from 'yup';
+import Select from 'react-select';
 
 const ReservationForm = () => {
   const initialValues = {
     name: '',
-    email: '',
     date: '',
-    time: '',
+    motorcycle: '',
+    location: null, // Initialize location as null
   };
+
+  const cities = [
+    { value: 'New York', label: 'New York' },
+    { value: 'Los Angeles', label: 'Los Angeles' },
+    { value: 'Chicago', label: 'Chicago' },
+    { value: 'Houston', label: 'Houston' },
+    { value: 'Miami', label: 'Miami' },
+    { value: 'london', label: 'london' },
+    { value: 'Tokyo', label: 'Tokyo' },
+    { value: 'Sydney', label: 'Sydney' },
+  ];
 
   const validationSchema = Yup.object().shape({
     name: Yup.string().required('Name is required'),
     date: Yup.date().required('Date is required'),
-    time: Yup.string().required('Time is required'),
     motorcycle: Yup.string().required('Motorcycle is required'),
+    location: Yup.object().nullable().required('Location is required'), // Update location validation
   });
 
   const handleSubmit = async (values, { resetForm }) => {
@@ -34,7 +46,7 @@ const ReservationForm = () => {
         validationSchema={validationSchema}
         onSubmit={handleSubmit}
       >
-        {() => (
+        {({ setFieldValue, values }) => (
           <Form>
             <div className="mb-4">
               <label htmlFor="name" className="block text-gray-600">Name:</label>
@@ -57,16 +69,6 @@ const ReservationForm = () => {
               <ErrorMessage name="date" component="div" className="text-red-600" />
             </div>
             <div className="mb-4">
-              <label htmlFor="time" className="block text-gray-600">Time:</label>
-              <Field
-                type="time"
-                id="time"
-                name="time"
-                className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
-              />
-              <ErrorMessage name="time" component="div" className="text-red-600" />
-            </div>
-            <div className="mb-4">
               <label htmlFor="motorcycle" className="block text-gray-600">Motorcycle:</label>
               <Field
                 type="text"
@@ -75,6 +77,18 @@ const ReservationForm = () => {
                 className="w-full px-4 py-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500"
               />
               <ErrorMessage name="motorcycle" component="div" className="text-red-600" />
+            </div>
+            <div className="mb-4">
+              <label htmlFor="location" className="block text-gray-600">Location:</label>
+              <Select
+                id="location"
+                name="location"
+                options={cities}
+                value={values.location}
+                onChange={(selectedOption) => setFieldValue('location', selectedOption)}
+                className="w-full border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <ErrorMessage name="location" component="div" className="text-red-600" />
             </div>
             <button
               type="submit"
