d2edd86b9225ffe94df1931fe314044d03678e20
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-26 11:08:23 -0600

    Add route to AddNew page

---
+++ src/routes/AddNew.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddNew from '../components/add/AddNew';
+
+export default function AddNewRoute() {
+  return (
+    <div className="h-screen flex flex-col justify-center align-center">
+      <AddNew />
+    </div>
+  );
+}
