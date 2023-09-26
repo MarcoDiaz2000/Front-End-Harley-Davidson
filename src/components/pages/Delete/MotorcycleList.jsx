04f44b24b94ccda0d896fb51e95a23d49ed6f035
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-09-26 06:19:04 -0600

    MotorcycleList

---
+++ src/components/pages/Delete/MotorcycleList.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchMotorcycles } from '../redux/motorcycles/motorcycles';
+import MotorcycleItem from './MotorcycleItem';
+
+const MotorcycleList = () => {
+  const dispatch = useDispatch();
+  const motorcycles = useSelector((state) => state.motorcycles.motorcycles);
+
+  useEffect(() => {
+    dispatch(fetchMotorcycles());
+  }, [dispatch]);
+
+  return (
+    <ul>
+      {motorcycles.map((motorcycle) => (
+        <MotorcycleItem key={motorcycle.id} motorcycle={motorcycle} />
+      ))}
+    </ul>
+  );
+};
+
+export default MotorcycleList;
