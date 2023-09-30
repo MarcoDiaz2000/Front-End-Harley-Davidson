4eca45b64d58e72922ef18a989347ac2cc14ba5d
Salimer <52242629+Salimer@users.noreply.github.com>
2023-09-30 13:24:31 +0300

    Update Details.jsx

--- src/components/motors/Details.jsx
+++ src/components/motors/Details.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams, Link } from 'react-router-dom';
 // import img from '../../images/motor2.png';
-import { fetchMotorcycleById } from '../../redux/motorcycles/motorcycles';
+import { fetchMotorcycleById } from '../../redux/bikes/motorcycles';
 
 export default function Details() {
   const { id } = useParams();
