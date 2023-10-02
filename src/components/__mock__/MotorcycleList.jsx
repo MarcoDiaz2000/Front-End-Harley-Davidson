cf09e8ad255f11fc0d3c2011b1a7cad26644e551
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-01 19:18:20 -0600

    Add mock for MotorcycleList

---
+++ src/components/__mock__/MotorcycleList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MotorcycleList = () => {
+  return (
+    <ul className="divide-customBorder">
+      <div className="text-center text-white font-bold">Without motorcycles in inventory</div>
+    </ul>
+  );
+};
+
+export default MotorcycleList;
