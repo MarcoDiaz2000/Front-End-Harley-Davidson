293c0b1f237d97875c3da782f1b1dfd1f24f3e95
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-01 19:27:58 -0600

    Fix linters

--- src/components/__mock__/MotorcycleList.jsx
+++ src/components/__mock__/MotorcycleList.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const MotorcycleList = () => {
-  return (
-    <ul className="divide-customBorder">
-      <div className="text-center text-white font-bold">Without motorcycles in inventory</div>
-    </ul>
-  );
-};
+const MotorcycleList = () => (
+  <ul className="divide-customBorder">
+    <div className="text-center text-white font-bold">Without motorcycles in inventory</div>
+  </ul>
+);
 
 export default MotorcycleList;
