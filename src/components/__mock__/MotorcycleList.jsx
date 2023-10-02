dc15e17afa42b2d3f438e836ca9dff97a2ee1135
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-02 06:50:33 -0600

    Complete tests

--- src/components/__mock__/MotorcycleList.jsx
+++ src/components/__mock__/MotorcycleList.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MotorcycleItem from './MotorcycleItem';
 
-const MotorcycleList = () => (
-  <ul className="divide-customBorder">
-    <div className="text-center text-white font-bold">Without motorcycles in inventory</div>
-  </ul>
-);
+export default function MotorcycleList({ bikes }) {
+  return (
+    <ul className="divide-customBorder">
+      {bikes.map((motorcycle) => (
+        <MotorcycleItem key={motorcycle.id} motorcycle={motorcycle} />
+      ))}
+    </ul>
+  );
+}
 
-export default MotorcycleList;
+MotorcycleList.propTypes = {
+  bikes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      removed: PropTypes.bool.isRequired,
+    }),
+  ).isRequired,
+};
--- src/test/delete/motorcycleList.test.js
+++ src/test/delete/motorcycleList.test.js
@@ -4,7 +4,12 @@ import MotorcycleList from '../../components/__mock__/MotorcycleList';
 
 describe('MotorcycleList', () => {
   it('renders correctly', () => {
-    const component = renderer.create(<MotorcycleList />);
+    const mockBikes = [
+      { id: 1, name: 'Bike1', removed: false },
+      { id: 2, name: 'Bike2', removed: true },
+    ];
+
+    const component = renderer.create(<MotorcycleList bikes={mockBikes} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
