1405ac05a00d672f460650fe95fa0dc4b0fe8770
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-10-03 15:22:49 +0500

    Create Pagination.jsx

---
+++ src/components/__mock__/Pagination.jsx
@@ -0,0 +1,6 @@
+// Mock data for Pagination component
+export const currentPageMock = 1;
+export const totalPagesMock = 3;
+
+// Mock function for onPageChange
+export const onPageChangeMock = jest.fn();
