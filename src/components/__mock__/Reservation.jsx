dfb9ab3feda8e12ea5507491fe0d42704dd4c2bf
Tamoor saeed <115831124+tamoorsaeed22@users.noreply.github.com>
2023-10-03 15:22:52 +0500

    Create Reservation.jsx

---
+++ src/components/__mock__/Reservation.jsx
@@ -0,0 +1,22 @@
+export const mockReservation = {
+  id: 1,
+  name: 'Sample Name',
+  username_id: 'sample-username',
+  item_id: 'sample-item-id',
+  city: 'Sample City',
+  date: '2023-10-03',
+  removed: false,
+};
+
+export const mockBikes = [
+  {
+    id: 1,
+    name: 'Bike 1',
+    description: 'Sample description',
+    price: 100,
+    image_url: 'sample-image-url',
+    category: 'Sample Category',
+    city: 'Sample City',
+  },
+  // Add more mock bikes as needed
+];
