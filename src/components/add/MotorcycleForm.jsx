47bf7f50ca579b1ba5b785fa3a89d804b4fe6b3f
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-04 07:21:06 -0600

    Clean input image after add new moto

--- src/components/add/MotorcycleForm.jsx
+++ src/components/add/MotorcycleForm.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function MotorcycleForm({ newMotor, handleChange, handleSubmit }) {
+function MotorcycleForm({
+  newMotor, handleChange, handleSubmit, imageInputRef,
+}) {
   return (
     <form onSubmit={(e) => handleSubmit(e)}>
       <div className="mb-3">
@@ -22,6 +24,7 @@ function MotorcycleForm({ newMotor, handleChange, handleSubmit }) {
         <label htmlFor="image" className="text-white block mb-2 font-roboto font-bold">
           Image *:
           <input
+            ref={imageInputRef}
             id="image"
             className="w-full p-2 rounded text-black font-roboto font-bold"
             type="file"
@@ -133,6 +136,10 @@ MotorcycleForm.propTypes = {
     total_amount_payable: PropTypes.number,
     duration: PropTypes.number,
   }).isRequired,
+  imageInputRef: PropTypes.oneOfType([
+    PropTypes.func,
+    PropTypes.shape({ current: PropTypes.instanceOf(Element) }),
+  ]).isRequired,
   handleChange: PropTypes.func.isRequired,
   handleSubmit: PropTypes.func.isRequired,
 };
