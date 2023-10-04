603f3566c72cc13487b70adcdc09d91f5f2c2f6e
MarcoDiaz2000 <md.marcodiaz2000@gmail.com>
2023-10-04 06:40:12 -0600

    Fix box

--- src/routes/Delete.jsx
+++ src/routes/Delete.jsx
@@ -33,10 +33,12 @@ export default function Delete() {
   }
 
   return (
-    <div className="flex justify-center bodyBg overflow-y-auto pt-12 pb-12 min-h-screen">
-      <div className="bg-customBg p-6 rounded-lg w-full sm:w-1/2 mb-12 white-shadow flex-shrink-0">
-        <h2 className="text-white text-3xl mb-4 font-helvetica font-bold">Delete Motorcycles</h2>
-        <MotorcycleList bikes={bikes} />
+    <div className="h-screen">
+      <div className="flex justify-center bodyBg overflow-y-auto pt-12 pb-12 min-h-screen">
+        <div className="bg-customBg p-5 rounded-lg w-full sm:w-1/2 mb-12 white-shadow flex-shrink-0">
+          <h2 className="text-white text-3xl mb-4 font-roboto font-bold">Delete Motorcycles</h2>
+          <MotorcycleList bikes={bikes} />
+        </div>
       </div>
     </div>
   );
